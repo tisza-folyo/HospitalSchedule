528483a3fd558587062cb47dfc5b28d79ae9973f
tisza <tiszalewi@gmail.com>
2026-02-17 20:13:25 +0100

    corsconfig created

---
+++ backend/src/main/java/com/hospital/backend/security/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.hospital.backend.security.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendUrl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
