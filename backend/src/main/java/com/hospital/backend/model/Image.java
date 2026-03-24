6cfda0d9e209933f10fc94939d16133c4f199e69
tisza <tiszalewi@gmail.com>
2026-03-24 23:11:42 +0100

    Jackson conversion problem fixes

--- backend/src/main/java/com/hospital/backend/model/Image.java
+++ backend/src/main/java/com/hospital/backend/model/Image.java
@@ -24,6 +24,7 @@ public class Image {
     private String dwnUrl;
 
     @Lob
+    @JsonIgnore
     private Blob image;
 
     @JsonIgnore
--- backend/src/main/java/com/hospital/backend/request/UpdateAssistantWorkRequest.java
+++ backend/src/main/java/com/hospital/backend/request/UpdateAssistantWorkRequest.java
@@ -1,5 +1,6 @@
 package com.hospital.backend.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,11 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class UpdateAssistantWorkRequest {
+    @JsonProperty("dTaj")
     String dTaj;
+    @JsonProperty("aTaj")
     String aTaj;
     LocalDate day;
+    @JsonProperty("uTaj")
     String uTaj;
 }
