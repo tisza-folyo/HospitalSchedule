fd814b62c39681c56fdd9b20c73f042469fe42b0
tisza <tiszalewi@gmail.com>
2026-03-25 23:50:53 +0100

    Jackson fixes

--- backend/src/main/java/com/hospital/backend/request/AddCareRequest.java
+++ backend/src/main/java/com/hospital/backend/request/AddCareRequest.java
@@ -1,5 +1,6 @@
 package com.hospital.backend.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hospital.backend.dto.BedDto;
 import com.hospital.backend.dto.NurseDto;
 import com.hospital.backend.dto.PatientDto;
@@ -12,10 +13,13 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class AddCareRequest {
+    @JsonProperty("pTaj")
     private String pTaj;
+    @JsonProperty("nTaj")
     private String nTaj;
     private long roomId;
     private int bedNumber;
+    @JsonProperty("uTaj")
     private String uTaj;
     private LocalDate entryDay;
 }
